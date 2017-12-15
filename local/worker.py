71716edf2aebaa10147500e3a41a14c84111d4b3
John Vinyard <john.vinyard@gmail.com>
2017-12-15 12:02:58 -0600

    Make it possible to start up multiple worker processes

--- local/worker.py
+++ local/worker.py
@@ -2,8 +2,11 @@ import urllib
 from autoencoder import most_recent_id, NoTrainedModelException
 from config import Sound, soundalike_client
 from learner import with_hash, Network
+from multiprocessing.pool import Pool
+import argparse
 
-if __name__ == '__main__':
+
+def work():
     recent_id = None
     cls = None
 
@@ -21,3 +24,20 @@ if __name__ == '__main__':
 
         cls.process(meta=metadata, _id=metadata.uri.url)
         print 'check it out at', urllib.quote(metadata.uri.url, safe='')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--processes',
+        type=int,
+        default=4,
+        help='how many workers should be launched in parallel?')
+    args = parser.parse_args()
+    pool = Pool(args.processes)
+
+    for i in xrange(args.processes):
+        pool.apply_async(work)
+
+    pool.close()
+    pool.join()
